e214629fb14d3a09f81bdf21140e956c84fc931f
RunpengJ <runpengj@gmail.com>
2022-11-15 18:01:08 -0800

    update grader

--- grade.sh
+++ grade.sh
@@ -1,7 +1,40 @@
 # Create your grading script here
-
+CPATH=".:../lib/hamcrest-core-1.3.jar:../lib/junit-4.13.2.jar"
 set -e
 
 rm -rf student-submission
 git clone $1 student-submission
 
+cp TestListExamples.java student-submission
+cd student-submission
+
+
+if [[ -e ListExamples.java ]]
+then
+  echo "ListExamples.java found"
+else
+  echo "ListExamples.java not found"
+  exit 1
+fi
+
+if [[ -f ListExamples.java ]] 
+then
+  echo "ListExamples.java is a file"
+else
+  echo "ListExamples.java is not a file"
+  exit 1
+fi
+
+set +e
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore TestListExamples > testout.txt
+echo "Report:"
+
+if [[ $? -eq 0 ]]
+then 
+  grep "OK" testout.txt
+  exit 0
+else
+  grep "Tests run:" testout.txt
+  exit 1
+fi
